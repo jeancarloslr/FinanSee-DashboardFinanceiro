6b08ff3c12578fd16bb98bd378436f2b35d6b18d
Jean Carlos <130818271+jeancarloslr@users.noreply.github.com>
2026-08-30 22:16:39 +0000

    opening modal to get user's new transation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
 //starting with dark/light theme.
 const toggle = document.querySelector('#theme-toggle');
 
+//starting to open the new transation modal
+const btnNewTransation = document.querySelector('.btn-nova-transacao')
+const modal = document.querySelector('.modal-nova-transacao')
+const btnCloseModal = document.querySelector('.close-transation')
+
+
+btnNewTransation.addEventListener('click', ()=>{
+   modal.showModal()
+});
+
+btnCloseModal.addEventListener('click', ()=>{
+   modal.close();
+});
+
  toggle.addEventListener('change', ()=>{
    document.body.classList.toggle("light-theme")
  })
